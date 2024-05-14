b43d6685c0722f7bc91967a4f29122451124c380
Yusup Hambali <sup.alpuket@gmail.com>
2024-05-15 04:30:22 +0700

    use `bun install` instead

--- bin.js
+++ bin.js
@@ -1,14 +1,21 @@
 #!/bin/env bun
 
-import mri from "mri";
 import tiged from "@yus-ham/tiged";
 import { resolve, basename } from "path";
+import { parseArgs } from "util";
 
-const argv = mri(process.argv, {
-    alias: { f: 'force', h: 'help' }
+
+const { values: options, positionals: argv } = parseArgs({ args: Bun.argv, strict: false })
+
+Object.entries({
+    f: 'force',
+    h: 'help',
+    p: 'production',
+    prod: 'production',
 })
+.forEach(([alias, opt]) => options[alias] && (options[opt] = true))
 
-if (argv.help || !argv._[2]) {
+if (options.help || !argv[2]) {
     console.info(
         'Usage:\n' +
         '  appinitr <source> [<directory>] [<options>]\n\n' +
@@ -17,27 +24,32 @@ if (argv.help || !argv._[2]) {
         '    - git url     Any git url\n\n' +
         '  Directory: download path destination. if ommited it will use current directory + project name\n\n' +
         '  Options:\n' +
-        '    --force    overwrite if destination directory exists\n' +
-        '    --help     Show this help\n'
+        '    -f, --force        Overwrite if destination directory exists\n' +
+        '    -p, --prod         Dont install devDependencies\n' +
+        '    -h, --help         Show this help\n'
     )
     process.exit()
 }
 
 try {
-    const dest = resolve(argv._[3] || basename(argv._[2] || ''))
+    const dest = resolve(argv[3] || basename(argv[2] || ''))
 
     console.info('Downloading project into directory:', dest)
 
-    await tiged(process.argv[2], {force: argv.force, keepVcs: true}).clone(dest)
-
-    const app_package_json = await import(`${dest}/package.json`)
-
-    for (const [name, script] of Object.entries(app_package_json.scripts)) {
-        if (name === 'app:init') {
-            await Bun.$`${script.split(' ')}`.cwd(dest)
-            break;
-        }
-    }
+    await import(`${dest}/package.json`).then(() => {
+        const { promise, resolve } = Promise.withResolvers()
+        const commands = ['bun', 'install']
+        options.production && commands.push('-p')
+        Bun.spawn(commands, {
+            stdout: 'inherit',
+            stdin: 'inherit',
+            onExit: resolve,
+            cwd: dest,
+        })
+        return promise
+    })
+    .catch((err) => {
+    })
 } catch(err) {
     console.error('Failed:')
     console.info('  '+ err.message.replace('Use options.force', 'Use --force') +'\n')
